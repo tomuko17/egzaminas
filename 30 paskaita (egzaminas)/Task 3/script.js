bdd9273a2d6779609f3d30581c96340ee340fb5e
Tomas Balciunas <tamasius@gmail.com>
2022-01-12 19:09:02 +0200

    3 padarytas

--- 30 paskaita (egzaminas)/Task 3/script.js
+++ 30 paskaita (egzaminas)/Task 3/script.js
@@ -11,3 +11,31 @@ Pastaba: Informacija apie user'į (jo kortelė) bei turi turėti bent minimalų 
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'https://api.github.com/users';
+
+document.querySelector("#btn").addEventListener('click',getData);
+
+function getData(){
+    fetch(ENDPOINT)
+    .then((Response)=>Response.json())
+       // return Response.text();
+       // document.querySelector("h1").textContent = login 
+   // })
+    .then ((data)=>{
+        //console.log(data);
+        data.forEach((item) => {
+        let myH=document.createElement("h4");
+        myH.append(`${item.login}`);
+        let myP=document.createElement("p");
+        myP.append(`${item.avatar_url}`);
+        document.querySelector("#output").append(myH, myP);
+        
+        });
+
+        //document.querySelector("h1").textContent=(duomenys);
+    })
+    document.querySelector("#message").textContent= "";
+    output.style.background ="cyan";
+       // .catch((err)=>{
+ //       console.log(err);
+  //  });
+    }
