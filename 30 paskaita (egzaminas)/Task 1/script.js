fdca261f444261a4d620a7644f336ebd1815adeb
Tomas Balciunas <tamasius@gmail.com>
2022-01-12 18:06:41 +0200

    liko isdestymas

--- 30 paskaita (egzaminas)/Task 1/script.js
+++ 30 paskaita (egzaminas)/Task 1/script.js
@@ -8,3 +8,22 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+document.querySelector("form").addEventListener("submit",(e)=>{
+e.preventDefault();
+ 
+    let Kg = document.querySelector("input").value;
+        console.log(Kg);
+    let svarai = Kg*2.2046;
+    console.log(svarai.toFixed(2));
+    let gramai = Kg*1000;
+    console.log(gramai);
+    let uncijos = Kg*35.274;
+    console.log(uncijos.toFixed(2));
+    document.querySelector("#output").textContent= ("Svoris svarai - "+svarai.toFixed(2) +" lb");
+    document.querySelector("#output").textContent= ("Svoris gramais - "+gramai +" g");
+  //  document.querySelector("#output").textContent= ("Svoris uncijomis - "+uncijos.toFixed(2) +" oz") ;
+
+
+});
+
